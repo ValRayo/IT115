9657a38837af5904949d4efe94cb9b7960ba928a
Valeria Munguia <Yamilexrayo21@gmail.com>
2023-11-09 15:47:52 -0800

    first commit

---
+++ json1.py
@@ -0,0 +1,40 @@
+#Class Library
+import json 
+
+#sample data
+data = {
+    'name': 'Osvaldo Guzman',
+    'age': 31,
+    'city': 'Seattle, WA',
+    'interests': ['Anime','Gym','Running'],
+    'is_student': False
+}
+
+
+#Writing data to a JSON file
+with open('data.json', 'w') as json_file:
+
+    json.dump(data, json_file, indent=4) 
+
+print('Data has been written to data.json')
+
+
+#Reading from the JSON file
+with open('data.json', 'r') as json_file:
+    #Load the data
+    loaded_data = json.load(json_file)
+
+print('Data loaded from data.json')
+print(loaded_data)
+
+
+    ##
+loaded_data['age'] = 22
+loaded_data['interests'].append('music')
+
+
+#
+with open('data.json', 'w') as json_file: 
+    json.dump(loaded_data, json_file, indent=4)
+
+print('Modified data to the data.json file')
